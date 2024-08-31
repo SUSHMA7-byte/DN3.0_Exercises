befa09e4ff3dd65865c248f8994da6a592da375c
SUSHMA S <116171643+SUSHMA7-byte@users.noreply.github.com>
2024-08-31 19:28:32 +0530

    Delete Week_4,5_Exercises/BookstoreAPI/PracticeCTSDN3 directory

--- Week_4,5_Exercises/BookstoreAPI/PracticeCTSDN3/src/main/java/com/demo/Document.java
+++
@@ -1,5 +0,0 @@
-package com.demo;
-
-public interface Document {
-    void open();
-}
--- Week_4,5_Exercises/BookstoreAPI/PracticeCTSDN3/src/main/java/com/demo/DocumentFactory.java
+++
@@ -1,6 +0,0 @@
-package com.demo;
-
-public abstract class DocumentFactory {
-    public abstract Document createDocument();
-}
-
